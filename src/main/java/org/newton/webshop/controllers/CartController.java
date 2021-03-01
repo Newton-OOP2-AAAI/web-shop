31074262cee480f040164939a499ac29238fc173
Alexander Wiklund <34245732+DoNotWorr@users.noreply.github.com>
2021-03-01 13:18:54 +0100

    finished cart controller

--- src/main/java/org/newton/webshop/controllers/CartController.java
+++ src/main/java/org/newton/webshop/controllers/CartController.java
@@ -3,10 +3,9 @@ package org.newton.webshop.controllers;
 import org.newton.webshop.exceptions.CartNotFoundException;
 import org.newton.webshop.models.Cart;
 import org.newton.webshop.repositories.CartRepository;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.Optional;
 
 @RestController
@@ -16,6 +15,19 @@ public class CartController {
     CartController(CartRepository repository) {
         this.repository = repository;
     }
+
+    @GetMapping("/carts")
+    List<Cart> all() {
+        return repository.findAll();
+    }
+
+    @PostMapping("/carts")
+    Cart newCart(@RequestBody Cart newCart) {
+        return repository.save(newCart);
+    }
+
+    // Single item
+
 
     /**
      * The customer wants to view content in the shopping cart (number of each article, price, name, size) to see what’s there.
@@ -27,5 +39,25 @@ public class CartController {
     Cart one(@PathVariable Integer id) {
         return repository.findById(id)
                 .orElseThrow(() -> new CartNotFoundException(id));
+    }
+
+    @PutMapping("/carts/{id}")
+    Cart replaceCart(@RequestBody Cart newCart, @PathVariable Integer id) {
+
+        return repository.findById(id)
+                .map(cart -> {
+                    cart.setItems(newCart.getItems());
+                    cart.setCustomer(newCart.getCustomer());
+                    return repository.save(cart);
+                })
+                .orElseGet(() -> {
+                    newCart.setId(id);
+                    return repository.save(newCart);
+                });
+    }
+
+    @DeleteMapping("/carts/{id}")
+    void deleteCart(@PathVariable Integer id) {
+        repository.deleteById(id);
     }
 }
