3aecb9da6c3160ad483330848931ef654269b591
Alexander Wiklund <34245732+DoNotWorr@users.noreply.github.com>
2021-03-16 15:11:18 +0100

    pojo start

--- src/main/java/org/newton/webshop/models/Review.java
+++ src/main/java/org/newton/webshop/models/Review.java
@@ -1,2 +1,35 @@
-package org.newton.webshop.models;public class Review {
+package org.newton.webshop.models;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+public class Review {
+    @Id @GeneratedValue(generator = "uuid2")
+    @GenericGenerator(name = "uuid2", strategy = "org.hibernate.id.UUIDGenerator")
+    private String id;
+
+    @OneToOne(mappedBy = "review")
+    @JoinColumn(name = "item_id")
+    private Item item;
+
+    @Column(nullable = false)
+    private Integer score;
+
+    @Column(nullable = false, length = 50)
+    private String title;
+
+    @Column(nullable = false, length = 500)
+    private String text;
+
+    @Column(nullable = false)
+    private LocalDate reviewDate;
 }
