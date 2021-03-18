6062f7e787786ca451e21a87196cb20d3106557a
IsaLjungdahl <isa.ljungdahl@gmail.com>
2021-03-18 11:19:25 +0100

    updated @onetomany in cart-model, added jsonIgnore,cascade and orphan removal

--- src/main/java/org/newton/webshop/models/Cart.java
+++ src/main/java/org/newton/webshop/models/Cart.java
@@ -1,12 +1,14 @@
 package org.newton.webshop.models;
 
-import com.fasterxml.jackson.annotation.JsonBackReference;
-import com.fasterxml.jackson.annotation.JsonManagedReference;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.*;
 import java.util.Set;
+
 @Getter
 @Setter
 @Table(name = "carts")
@@ -17,11 +19,14 @@ public class Cart {
     @Column(name = "cart_id")
     private Integer id;
 
-    @JsonManagedReference
-    @OneToMany(mappedBy = "cart")
+
+    @OneToMany(cascade = CascadeType.ALL,
+            orphanRemoval = true,
+            mappedBy = "cart")
+    @JsonIgnore
     private Set<Item> items;
 
-    @JsonBackReference
+
     @OneToOne
     @JoinColumn(name = "customer_id", nullable = false)
     private Customer customer;
