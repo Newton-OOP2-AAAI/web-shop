8cf10a9ccee325732f5bff930c3b886deba30b85
Alexander Wiklund <34245732+DoNotWorr@users.noreply.github.com>
2021-03-23 17:44:59 +0100

    Added Service between Repository and Controller for Question/Answer entities

--- src/main/java/org/newton/webshop/exceptions/AnswerNotFoundException.java
+++ src/main/java/org/newton/webshop/exceptions/AnswerNotFoundException.java
@@ -1,7 +1,7 @@
 package org.newton.webshop.exceptions;
 
 public class AnswerNotFoundException extends RuntimeException {
-    public AnswerNotFoundException(Integer id) {
+    public AnswerNotFoundException(String id) {
         super("Could not find the answer " + id);
     }
 }
--- src/main/java/org/newton/webshop/exceptions/QuestionNotFoundException.java
+++ src/main/java/org/newton/webshop/exceptions/QuestionNotFoundException.java
@@ -1,7 +1,7 @@
 package org.newton.webshop.exceptions;
 
 public class QuestionNotFoundException extends RuntimeException {
-    public QuestionNotFoundException(Integer id) {
+    public QuestionNotFoundException(String id) {
         super("Could not find the question " + id);
     }
 }
