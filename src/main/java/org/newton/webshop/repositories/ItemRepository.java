f8ff970c31254321e7fc499b085a5653aaef9810
IsaLjungdahl <isa.ljungdahl@gmail.com>
2021-03-24 16:33:07 +0100

    autoformatted everything

--- src/main/java/org/newton/webshop/repositories/ItemRepository.java
+++ src/main/java/org/newton/webshop/repositories/ItemRepository.java
@@ -2,6 +2,7 @@ package org.newton.webshop.repositories;
 
 import org.newton.webshop.models.entities.Item;
 import org.springframework.data.repository.CrudRepository;
+
 import java.util.List;
 
 public interface ItemRepository extends CrudRepository<Item, String> {
