23c1cd0b6e4fb576671fcdf861bcc4b17c06cae7
andrkavr <72137459+andrkavr@users.noreply.github.com>
2021-03-30 09:40:32 +0200

    Commented InventoryController. Hotfix.

--- src/main/java/org/newton/webshop/rest/ItemController.java
+++ src/main/java/org/newton/webshop/rest/ItemController.java
@@ -17,30 +17,30 @@ public class ItemController {
         this.itemService = itemService;
     }
 
-    @GetMapping("/all")
-    List<Item> all() {
-        return itemService.findAll();
-    }
-
-    @PostMapping
-    Item newItem(@RequestBody Item newItem) {
-        return itemService.newItem(newItem);
-    }
-
-    // Single item
-
-    @GetMapping
-    Item one(@RequestParam String id) {
-        return itemService.findById(id);
-    }
-
-    @PutMapping
-    Item replaceItem(@RequestBody Item newItem, @RequestParam String id) {
-        return itemService.replaceItem(newItem, id);
-    }
-
-    @DeleteMapping
-    void deleteItem(@RequestParam String id) {
-        itemService.deleteItemById(id);
-    }
+//    @GetMapping("/all")
+//    List<Item> all() {
+//        return itemService.findAll();
+//    }
+//
+//    @PostMapping
+//    Item newItem(@RequestBody Item newItem) {
+//        return itemService.newItem(newItem);
+//    }
+//
+//    // Single item
+//
+//    @GetMapping
+//    Item one(@RequestParam String id) {
+//        return itemService.findById(id);
+//    }
+//
+//    @PutMapping
+//    Item replaceItem(@RequestBody Item newItem, @RequestParam String id) {
+//        return itemService.replaceItem(newItem, id);
+//    }
+//
+//    @DeleteMapping
+//    void deleteItem(@RequestParam String id) {
+//        itemService.deleteItemById(id);
+//    }
 }
