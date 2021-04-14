749d21740e1de586de9ea2e65ba3ff9524854938
Abdi Hussein <abdi_665@hotmail.com>
2021-04-14 20:38:06 +0200

    Fixed endpoints and added java docs on the controllers.

--- src/main/java/org/newton/webshop/rest/StaffController.java
+++ src/main/java/org/newton/webshop/rest/StaffController.java
@@ -7,6 +7,7 @@ import org.newton.webshop.models.dto.response.RoleDto;
 import org.newton.webshop.models.dto.update.EmployeeUpdateDto;
 import org.newton.webshop.services.StaffService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -22,48 +23,101 @@ public class StaffController {
         this.staffService = staffService;
     }
 
-    @PostMapping
+    /**
+     * Create a employee
+     *
+     * @param creationDto dto
+     * @return employee dto
+     */
+    @PostMapping(produces = MediaType.APPLICATION_JSON_VALUE)
     public EmployeeDto createEmployee(@RequestBody EmployeeCreationDto creationDto) {
         return staffService.createEmployee(creationDto);
     }
 
-    @PutMapping
-    public EmployeeDto updateEmployeeById(@RequestParam String id, @RequestBody EmployeeUpdateDto employeeUpdateDto) {
-        return staffService.updateEmployee(id, employeeUpdateDto);
+    /**
+     * Update a employee
+     *
+     * @param employeeId        id of employee to update
+     * @param employeeUpdateDto dto containing fields that will be updated
+     * @return employee dto
+     */
+    @PutMapping(path = "/{employee_id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public EmployeeDto updateEmployeeById(@PathVariable(name = "employee_id") String employeeId,
+                                          @RequestBody EmployeeUpdateDto employeeUpdateDto) {
+        return staffService.updateEmployee(employeeId, employeeUpdateDto);
     }
 
-    @GetMapping
+    /**
+     * Find employee by id
+     *
+     * @param id category id
+     * @return employee dto
+     */
+    @GetMapping(params = {"id"}, produces = MediaType.APPLICATION_JSON_VALUE)
     public EmployeeDto findEmployeeById(@RequestParam String id) {
         return staffService.findById(id);
     }
 
-    @GetMapping("/all")
+    /**
+     * Find all employees
+     *
+     * @return employee dto
+     */
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
     public List<EmployeeDto> findAllEmployees() {
         return staffService.findAll();
     }
 
-    @DeleteMapping
-    public void deleteEmployeeById(@RequestParam String employeeId) {
+    /**
+     * Delete a employee
+     *
+     * @param employeeId id of employee to delete
+     */
+    @DeleteMapping(path = "/{employee_id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public void deleteEmployeeById(@PathVariable(name = "employee_id") String employeeId) {
         staffService.deleteEmployeeById(employeeId);
     }
 
-    @GetMapping("/roles")
+    /**
+     * Find all roles
+     *
+     * @return role dto
+     */
+    @GetMapping(path = "/roles")
     public List<RoleDto> findAllRoles() {
         return staffService.findAllRoles();
     }
 
-    @PostMapping("/role/create")
+    /**
+     * Create a role
+     *
+     * @param creationDto dto
+     * @return role dto
+     */
+    @PostMapping(path = "/role", produces = MediaType.APPLICATION_JSON_VALUE)
     public RoleDto createRole(@RequestBody RoleCreationDto creationDto) {
         return staffService.createRole(creationDto);
     }
 
-    @PutMapping("/role/{roleId}")
-    public RoleDto updateRoleById(@PathVariable String roleId, @RequestBody RoleCreationDto updateRole) {
+    /**
+     * Update a role
+     *
+     * @param roleId     id of role to update
+     * @param updateRole dto containing fields that will be updated
+     * @return role dto
+     */
+    @PutMapping(path = "/roles/{role_id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public RoleDto updateRoleById(@PathVariable(name = "role_id") String roleId, @RequestBody RoleCreationDto updateRole) {
         return staffService.updateRole(roleId, updateRole);
     }
 
-    @DeleteMapping("/roles/{roleId}")
-    public void deleteRoleById(@PathVariable String roleId) {
+    /**
+     * Delete a role
+     *
+     * @param roleId id of role to delete
+     */
+    @DeleteMapping(path = "/roles/{role_id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public void deleteRoleById(@PathVariable(name = "role_id") String roleId) {
         staffService.deleteRoleById(roleId);
     }
 }
